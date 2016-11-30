4737b4b040b019f9020fa759edd45dcb05a2588c
Hope Scheffert <hope_scheffert@domain.com>
2016-11-30 14:58:44 -0600

    working on adding category

--- MyPantry2.0/app/src/main/java/com/example/z/mypantry20/CategoryOverviewView.java
+++ MyPantry2.0/app/src/main/java/com/example/z/mypantry20/CategoryOverviewView.java
@@ -2,6 +2,7 @@ package com.example.z.mypantry20;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -42,46 +43,49 @@ public class CategoryOverviewView extends AppCompatActivity
             public void onClick(View v)
             {
 
+
+                Intent i = new Intent(CategoryOverviewView.this, AddCategoryActivity.class);
+                startActivity(i);
+
                 //TODO pop up window for user to type in name, description then adds a category
-
-                LayoutInflater factory = LayoutInflater.from(getBaseContext());
-                final View textEntryView = factory.inflate(R.layout.new_category_prompt, null);
-                final EditText name = (EditText) textEntryView.findViewById(R.id.name);
-                final EditText description = (EditText) textEntryView.findViewById(R.id.description);
-                name.setText("Name", TextView.BufferType.EDITABLE);
-                description.setText("Description", TextView.BufferType.EDITABLE);
-                final AlertDialog.Builder alert = new AlertDialog.Builder(getBaseContext());
-
-                alert.setTitle("Add a new Category")
-                        .setView(textEntryView)
-                        .setPositiveButton("Save",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                        Log.i("AlertDialog","TextEntry 1 Entered "+ name.getText().toString());
-                                        Log.i("AlertDialog","TextEntry 2 Entered "+ description.getText().toString());
-
-                                         /* User clicked OK so do some stuff */
-
-                                        Context context = getApplicationContext();
-                                        CharSequence text = "Added Category";
-                                        Toast t = Toast.makeText(context, text, Toast.LENGTH_SHORT);
-                                        t.show();
-
-                                        //TODO create pantry item list for category constructor?
-//                                        List<PantryItem> pantryItemList = new List<PantryItem>();
-//                                        pantryItemList.add(new PantryItem("milk", 10, "oz"));
-
-
-                                        Category newCategory = new Category(null, name.getText().toString(), description.getText().toString());
-                                        categories.add(newCategory);
-                                    }
-                                })
-                        .setNegativeButton("Cancel",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialog, int whichButton) {
-                                    }
-                                });
-                alert.show();
+//                LayoutInflater factory = LayoutInflater.from(getBaseContext());
+//                final View textEntryView = factory.inflate(R.layout.new_category_prompt, null);
+//                final EditText name = (EditText) textEntryView.findViewById(R.id.name);
+//                final EditText description = (EditText) textEntryView.findViewById(R.id.description);
+//                name.setText("Name", TextView.BufferType.EDITABLE);
+//                description.setText("Description", TextView.BufferType.EDITABLE);
+//                final AlertDialog.Builder alert = new AlertDialog.Builder(getBaseContext());
+//
+//                alert.setTitle("Add a new Category")
+//                        .setView(textEntryView)
+//                        .setPositiveButton("Save",
+//                                new DialogInterface.OnClickListener() {
+//                                    public void onClick(DialogInterface dialog, int whichButton) {
+//                                        Log.i("AlertDialog","TextEntry 1 Entered "+ name.getText().toString());
+//                                        Log.i("AlertDialog","TextEntry 2 Entered "+ description.getText().toString());
+//
+//                                         /* User clicked OK so do some stuff */
+//
+//                                        Context context = getApplicationContext();
+//                                        CharSequence text = "Added Category";
+//                                        Toast t = Toast.makeText(context, text, Toast.LENGTH_SHORT);
+//                                        t.show();
+//
+//                                        //TODO create pantry item list for category constructor?
+////                                        List<PantryItem> pantryItemList = new List<PantryItem>();
+////                                        pantryItemList.add(new PantryItem("milk", 10, "oz"));
+//
+//
+//                                        Category newCategory = new Category(null, name.getText().toString(), description.getText().toString());
+//                                        categories.add(newCategory);
+//                                    }
+//                                })
+//                        .setNegativeButton("Cancel",
+//                                new DialogInterface.OnClickListener() {
+//                                    public void onClick(DialogInterface dialog, int whichButton) {
+//                                    }
+//                                });
+//                alert.show();
 
             }
         });
