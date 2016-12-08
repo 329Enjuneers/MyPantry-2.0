f8a1ead8e025efe2b964fd1c78a9f7b99ac30a35
Joseph McGovern <joseph.mcgovern@webfilings.com>
2016-12-08 11:18:49 -0600

    update db version

--- MyPantry2.0/app/src/main/java/com/example/z/mypantry20/PantryDbHelper.java
+++ MyPantry2.0/app/src/main/java/com/example/z/mypantry20/PantryDbHelper.java
@@ -40,7 +40,7 @@ public class PantryDbHelper extends SQLiteOpenHelper {
             "DROP TABLE IF EXISTS " + PantryContract.Recipe.TABLE_NAME;
 
     // If you change the database schema, you must increment the database version.
-    public static final int DATABASE_VERSION = 5;
+    public static final int DATABASE_VERSION = 6;
     public static final String DATABASE_NAME = "Pantry.db";
 
     public PantryDbHelper(Context context) {
